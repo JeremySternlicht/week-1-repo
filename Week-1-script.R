e63d631afcc6627b23ca463d27581824161fce82
Your Name <you@example.com>
2023-09-28 22:08:13 -0700

    I've now added a new data frame, "hhmem", which contains the data on the number of listed household members for the entire sample. Additionally, I've used "ggplot" to create a histogram that plots the distribution of this data throughout the sample.

--- Week-1-script.R
+++ Week-1-script.R
@@ -1,5 +1,15 @@
 library(tidyverse)
 library(haven)
+library(ggplot2)
 
 nfhs <- read_dta("IAHR52FL.dta")
 hh <- select(nfhs, hhid:shstruc)
+hhmem <- select(nfhs, hv009)
+
+ggplot(data = hhmem, aes(x = hv009)) +
+  geom_histogram(binwidth = 1, fill = "blue", color = "black") +
+  labs(
+    title = "Distribution of Listed Household Members",
+    x = "Number of Household Members",
+    y = "Frequency"
+  )
