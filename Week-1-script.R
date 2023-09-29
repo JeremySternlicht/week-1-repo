e713f1372b624e36755f9c051026dab9de6b2469
Your Name <you@example.com>
2023-09-28 22:34:59 -0700

    In lines 16-26, I've created a new data frame that contains only urban households. Then, I've used the "factor" function to translate the categorical variables for R from "0, 1, 2" to "Large City, Small City, Town". I've also created a boxplot that shows the distribution of household size by type of urban area.

--- Week-1-script.R
+++ Week-1-script.R
@@ -13,3 +13,14 @@ ggplot(data = hhmem, aes(x = hv009)) +
     x = "Number of Household Members",
     y = "Frequency"
   )
+
+hhurban <- filter (nfhs, hv025 == 1)
+hhurban$UrbanType <- factor(hhurban$hv026, levels = c(0, 1, 2), labels = c("Large City", "Small City", "Town"))
+
+ggplot(data = hhurban, aes(x = UrbanType, y = hv009)) +
+  geom_boxplot(fill = "blue", color = "black") +
+  labs(
+    title = "Distribution of Household Size by Urban Area Type",
+    x = "Urban Area Type",
+    y = "Household Size"
+  )
